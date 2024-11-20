d88f848215aff74602922684ba37c44b6fcb1c21
ThorBeck20 <65478692+ThorBeck20@users.noreply.github.com>
2024-11-20 15:49:04 -0800

    essential files - delete later

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mymapsapplication
+package com.example.watcomtravels
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -13,48 +13,80 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mymapsapplication.ui.theme.MyMapsApplicationTheme
-import com.google.android.gms.maps.CameraUpdate
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.internal.IGoogleMapDelegate
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.rememberCameraPositionState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val bham = LatLng(48.73, -122.47)
-            val cameraPostionState = rememberCameraPositionState {
-                position = CameraPosition.fromLatLngZoom(bham, 15f)
-            }
-            Box (
-                modifier = Modifier.fillMaxSize()
-            ) {
-                GoogleMap(
-                    modifier = Modifier.fillMaxSize(),
-                    cameraPositionState = cameraPostionState
-                )
+            var stopIds : List<StopObject> = emptyList()
+            val currentLocation = 1
+            val bham = LatLng(48.73, -122.49)
 
-                Button(
-                    onClick = {
-                        cameraPostionState.move(
-                            CameraUpdateFactory.newCameraPosition(
-                                CameraPosition.fromLatLngZoom(
-                                    LatLng(48.0, -122.0),
-                                    10f
-                                )
-                            )
-                        )
-                    },
-                    modifier = Modifier.align(Alignment.BottomStart)
-                ) {
-                    Text("Move")
+            LaunchedEffect(true) {
+                withContext(Dispatchers.IO) {
+                    stopIds = WTAApi.getStopObjets()
                 }
             }
+            Column (
+                modifier = Modifier.fillMaxSize()
+            ) {
+                BasicMap()
+//                CoolMap(startingLocation = bham)
+//                for (stop in stopIds) {
+//                    Text(text = stop.id.toString())
+//                }
+            }
+
         }
     }
+}
+
+@Composable
+fun BasicMap() {
+    val bham = LatLng(48.73, -122.47)
+    val cameraPostionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(bham, 15f)
+    }
+    Box (
+        modifier = Modifier.fillMaxSize()
+    ) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPostionState
+        )
+
+        Button(
+            onClick = {
+                cameraPostionState.move(
+                    CameraUpdateFactory.newCameraPosition(
+                        CameraPosition.fromLatLngZoom(
+                            LatLng(48.0, -122.0),
+                            10f
+                        )
+                    )
+                )
+            },
+            modifier = Modifier.align(Alignment.BottomStart)
+        ) {
+            Text("Move")
+        }
+    }
+}
+
+@Composable
+fun CoolMap(startingLocation: LatLng) {
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(startingLocation, 15f)
+    }
+    val markerState = rememberMarkerState(position = LatLng(48.73, -122.49))
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        AdvancedMarker(
+            state = markerState,
+            title = "Communications Facility"
+        )
+    }
+}
 }
